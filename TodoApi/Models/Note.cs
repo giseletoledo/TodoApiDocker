98303be656916baa5ab3b07cd207c8a537b130dc
Gisele <gisesonia@gmail.com>
2024-10-04 21:11:54 -0300

    model

--- TodoApi/Models/Note.cs
+++ TodoApi/Models/Note.cs
@@ -3,7 +3,7 @@ namespace TodoApi.Models
     public class Note
     {
         public int Id { get; set; }
-        public required string Description { get; set; }
+        public string Description { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
