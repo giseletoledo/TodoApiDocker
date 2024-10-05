8a4019b4ff06daa81e3bfe72c67731ebd9ff83ee
Gisele <gisesonia@gmail.com>
2024-10-05 14:41:08 -0300

    corrigindo o dto

--- TodoApi/DTOs/NoteDto.cs
+++ TodoApi/DTOs/NoteDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace TodoApi.DTOs
     public class NoteDto
     {
         public int Id { get; set; }
-        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; } = string.Empty;
     }
 }
